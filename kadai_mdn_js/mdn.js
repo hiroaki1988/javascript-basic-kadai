6658cb68fc88d2b399d8aa96cfb50c11e591419a
hiroaki1988 <153973404+hiroaki1988@users.noreply.github.com>
2024-02-18 11:29:58 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,6 @@
+const date = new Date([2024,2,18]);
+const month = date.getMonth() + 1;
+const day = date.getDate();
+const year = date.getFullYear();
+
+console.log(year +"年" + month + "月" + day + "日");
